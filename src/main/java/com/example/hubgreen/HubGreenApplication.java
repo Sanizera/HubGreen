4c01c99a14d7f89183c305f8858be18637be92b4
Nicolas Schonardie <nic.discord0403@gmail.com>
2026-06-18 20:47:36 -0300

    Project base

---
+++ src/main/java/com/example/hubgreen/HubGreenApplication.java
@@ -0,0 +1,13 @@
+package com.example.hubgreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubGreenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HubGreenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hubgreen/HubGreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hubgreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubGreenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
