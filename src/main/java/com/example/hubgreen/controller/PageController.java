fc223277400b653f9059e844d60983b6f3c8ff4f
Sanizera <nic.discord0403@gmail.com>
2026-07-10 20:47:20 -0300

    BackEnd - Criação de endpoints direcionando para as páginas Fixes #5

---
+++ src/main/java/com/example/hubgreen/controller/PageController.java
@@ -0,0 +1,31 @@
+package com.example.hubgreen.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@Controller
+public class PageController {
+    @GetMapping("/search")
+    public String search() {
+        return "search";
+    }
+    
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/event")
+    public String event() {
+        return "event";
+    }
+    
+    @GetMapping("/profile")
+    public String profile() {
+        return "profile";
+    }
+    
+}
